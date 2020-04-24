e7449cc316480cd648c4cafa4a8001bfc5d4155a
zbeibei31 <55460717+zbeibei31@users.noreply.github.com>
2020-04-23 22:03:50 -0400

    Add files via upload

---
+++ src/components/Logo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+
+
+
+const Logo=(props)=>{
+    return(
+        <div className="name">
+            <a href="#">My Favorite Movies</a>
+        </div>
+    )
+} 
+
+export default Logo
